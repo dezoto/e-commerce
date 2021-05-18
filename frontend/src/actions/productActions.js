59d57677d7d6d5da6981acc1c2862649afafc777
dezoto <dyruzmbk@gmail.com>
2021-05-18 18:27:27 +0700

    Defined new action for product details page
    added redux to product detail screen, added product detail reducer, defined new action to fetch data from backend and display on screen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,18 +1,33 @@
 import axios from "axios";
-import {
-    PRODUCT_LIST_FAIL,
-  PRODUCT_LIST_REQUEST,
-  PRODUCT_LIST_SUCCESS,
-} from "../constants/productConstants";
+import * as actions from "../constants/productConstants";
 
 export const listProducts = () => async (dispatch) => {
   dispatch({
-    type: PRODUCT_LIST_REQUEST,
+    type: actions.PRODUCT_LIST_REQUEST,
   });
   try {
     const { data } = await axios.get("/api/products");
-    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    dispatch({ type: actions.PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
-      dispatch({type: PRODUCT_LIST_FAIL, payload: error.message })
+    dispatch({ type: actions.PRODUCT_LIST_FAIL, payload: error.message });
+  }
+};
+
+export const detailsProduct = (productId) => async (dispatch) => {
+  dispatch({
+    type: actions.PRODUCT_DETAILS_REQUEST,
+    payload: productId,
+  });
+  try {
+    const { data } = await axios.get(`/api/products/${productId}`);
+    dispatch({ type: actions.PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: actions.PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,14 +1,33 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants";
+import * as actions from "../constants/productConstants";
 
-export const productListReducer = (state = { loading: true, products: [] }, action) => {
-    switch(action.type) {
-        case PRODUCT_LIST_REQUEST:
-            return {loading: true};
-        case PRODUCT_LIST_SUCCESS:
-            return {loading: false, products: action.payload};
-        case PRODUCT_LIST_FAIL:
-            return {loading: false, error: action.payload};
-        default:
-            return state;
-    }
-}
+export const productListReducer = (
+  state = { loading: true, products: [] },
+  action
+) => {
+  switch (action.type) {
+    case actions.PRODUCT_LIST_REQUEST:
+      return { loading: true };
+    case actions.PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    case actions.PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product: {}, loading: true },
+  action
+) => {
+  switch (action.type) {
+    case actions.PRODUCT_DETAILS_REQUEST:
+      return { loading: true };
+    case actions.PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case actions.PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
