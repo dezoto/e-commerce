d483b7c7f6a98eb4b2061f98058db25403a3a842
dezoto <dyruzmbk@gmail.com>
2021-05-19 16:31:32 +0700

    Created hook to get product qty
    Created cart screen, function to handle add to cart, redirect user to cart screen, select box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
-import ProductScreen from './screens/ProductScreen';
-import HomeScreen from './screens/HomeScreen';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className='grid-container'>
-        <header className='row'>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              DoubleSix
+            </a>
+          </div>
+          <div className="right-nav">
             <div>
-                <a className='brand' href='/'>DoubleSix</a>
+              <a href="/cart">Cart</a>
             </div>
-            <div className='right-nav'>
-                <div><a href='/cart'>Cart</a></div>
-                <div className='signin'><a href='/signin'>Sign In</a></div>
+            <div className="signin">
+              <a href="/signin">Sign In</a>
             </div>
+          </div>
         </header>
         <main>
-          <Route path='/product/:id' component={ProductScreen}></Route>
-          <Route path='/' component={HomeScreen} exact></Route>  
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
         </main>
-        <footer className='row center'>
-            All right reserved
-        </footer>
-    </div>
+        <footer className="row center">All right reserved</footer>
+      </div>
     </BrowserRouter>
   );
 }
